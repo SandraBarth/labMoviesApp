1f50b27ba0c973fd1197f32c633118fc28a7c15b
SandraBarth <s2010238005@fhooe.at>
2022-10-20 15:24:38 +0100

    Fix backward/forward arrows.

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -5,49 +5,26 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
-import Avatar from "@mui/material/Avatar";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import { CardHeader } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const MovieHeader = (props) => {
   const movie = props.movie;
-  const favouriteMovies = JSON.parse(localStorage.getItem("favourites"));
-  let isFavourite = false; 
-  (() => {
-    for (let i = 0; i < favouriteMovies.length; i++) {
-      if(movie.id === favouriteMovies[i].id){
-        isFavourite = true;
-      }
-    }
-  })();
- 
+  const navigate = useNavigate();
 
   return (
-    <Paper
-      component="div"
-      sx={{
-        display: "flex",
-        justifyContent: "space-around",
-        flexWrap: "wrap",
-        padding: 1.5,
-        margin: 0,
-      }}
-    >
-      <IconButton aria-label="go back">
+    <Paper 
+        component="div" 
+        sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            flexWrap: "wrap",
+            padding: 1.5,
+            margin: 0,
+        }}
+      >
+      <IconButton aria-label="go back" onClick={() => navigate(-1)} >
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
-
-      {
-        (isFavourite) ?
-          (<CardHeader aria-label="favourite"
-            avatar={
-              < Avatar sx={{ backgroundColor: 'red' }}>
-                <FavoriteIcon />
-              </Avatar>}
-          />) : (
-            null
-          )
-      }
 
       <Typography variant="h4" component="h3">
         {movie.title}
@@ -57,7 +34,8 @@ const MovieHeader = (props) => {
         <br />
         <span sx={{ fontSize: "1.5rem" }}>{`   "${movie.tagline}"`} </span>
       </Typography>
-      <IconButton aria-label="go forward">
+
+      <IconButton aria-label="go forward" onClick={() => navigate(+1) } >
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
