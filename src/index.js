a70004515518eeb9d8fad430ed8d3647c16e6011
SandraBarth <s2010238005@fhooe.at>
2022-10-20 15:09:54 +0100

    Full review page.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import favouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import MovieReviewPage from "./pages/movieReviewPage";
 
 const App = () => {
   return (
@@ -21,11 +22,12 @@ const App = () => {
         <Route exact path="/movies/favourites" element={<favouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
-        <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/reviews/:id" element={<MovieReviewPage />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
-const rootElement = createRoot( document.getElementById("root") )
-rootElement.render(<App /> );
+const rootElement = createRoot(document.getElementById("root"))
+rootElement.render(<App />);
