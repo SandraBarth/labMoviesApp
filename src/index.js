b7910ab4c5c160d5a9d57231376390c6de9747cc
SandraBarth <s2010238005@fhooe.at>
2022-10-28 10:53:51 +0100

    Using react-query to manage the browser-based cache.

--- src/index.js
+++ src/index.js
@@ -2,28 +2,46 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
-import MoviePage from "./pages/movieDetailsPage";
+import MovieDetailsPage from "./pages/movieDetailsPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import SiteHeader from './components/siteHeader'; 
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools';
+import MoviesContextProvider from "./contexts/moviesContext";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
 
 const App = () => {
-  return (
+    return (
+      <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-          <SiteHeader />      {/* New Header  */}
+      <SiteHeader />      {/* New Header  */}
+      <MoviesContextProvider>
           <Routes>
         <Route direct path="/movies/favourites" element={<FavouriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/movies/:id" element={<MovieDetailsPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
         <Route path="/reviews/:id" element={<MovieReviewPage />} />
         <Route path = "/movies/upcoming" element = {<UpcomingMoviesPage /> } /> 
       </Routes>
+      </MoviesContextProvider>
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
-const rootElement = createRoot(document.getElementById("root"))
+const rootElement = createRoot(document.getElementById("root"));
 rootElement.render(<App />);
